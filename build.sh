420ca561ab415ce9deb29d9cd6a487226fb90e87
Lei Wang <tendant@gmail.com>
2018-05-30 11:39:11 -0700

    add tailwind theme

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/tailwind build ./static/css/styles.css -c ./tailwind.js -o ./static/css/output.css
+./node_modules/.bin/tailwind build ./themes/tailwind/static/css/styles.css -c ./tailwind.js -o ./themes/tailwind/static/css/output.css
